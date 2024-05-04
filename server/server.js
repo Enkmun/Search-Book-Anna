56eadc99fcbf8e21ba765fa8e32d3060a13aa65a
Enkmun <147563607+Enkmun@users.noreply.github.com>
2024-05-04 18:04:53 -0400

    updated queries,server, and add readme

--- server/server.js
+++ server/server.js
@@ -9,13 +9,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/dist as static assets
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
+// Routes
 app.use(routes);
 
+// Database connection and server start
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
